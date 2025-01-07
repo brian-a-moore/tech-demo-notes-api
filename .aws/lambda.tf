4c441dbac901743adca35afcdb87eed8774fba04
Brian Moore <brianmoore0213@gmail.com>
2025-01-07 18:54:37 -0500

    updated terraform and github to work with serverless

--- .aws/lambda.tf
+++ .aws/lambda.tf
@@ -1,9 +1,11 @@
 resource "aws_lambda_function" "folder_service" {
-  function_name = "folder_service"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  timeout       = 10
+  function_name    = "folder_service"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  filename         = "./.serverless/folder_service.zip"
+  source_code_hash = filebase64sha256("./.serverless/folder_service.zip")
+  runtime          = "nodejs18.x"
+  timeout          = 10
 
   environment {
     variables = {
@@ -13,11 +15,13 @@ resource "aws_lambda_function" "folder_service" {
 }
 
 resource "aws_lambda_function" "notes_service" {
-  function_name = "notes_service"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  timeout       = 10
+  function_name    = "notes_service"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  filename         = "./.serverless/note_service.zip"
+  source_code_hash = filebase64sha256("./.serverless/note_service.zip")
+  runtime          = "nodejs18.x"
+  timeout          = 10
 
   environment {
     variables = {
