4704fca12d23a527874a8d90520daeee990bd754
Brian Moore <brianmoore0213@gmail.com>
2025-01-08 16:25:29 -0500

    updates to work with logs

--- .aws/iam.tf
+++ .aws/iam.tf
@@ -1,3 +1,29 @@
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "IAM policy for Lambda functions to access CloudWatch Logs and DynamoDB"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+          "dynamodb:*"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.notes_api_database.arn
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
   assume_role_policy = jsonencode({
@@ -12,4 +38,9 @@ resource "aws_iam_role" "lambda_role" {
       },
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
