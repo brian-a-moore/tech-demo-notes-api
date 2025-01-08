6ad4d139ee8d9447ed4948a88a321eea7e28768a
Brian Moore <brianmoore0213@gmail.com>
2025-01-07 20:37:56 -0500

    prettier and eslint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,41 @@
-import pluginJs from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-/** @type {import('eslint').Linter.Config[]} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  { env: { node: true } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier,
+  ...compat.extends('eslint:recommended', 'prettier', 'plugin:@typescript-eslint/recommended'),
   {
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended",
-    ],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
 ];
