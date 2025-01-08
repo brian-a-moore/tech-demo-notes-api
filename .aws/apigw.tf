c93ec3717db1bfb534039a4b288bb0e3d223ff28
Brian Moore <brianmoore0213@gmail.com>
2025-01-07 21:06:19 -0500

    moved some things round with apigw

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -1,45 +1,10 @@
-variable "rest_api_id" {}
-variable "parent_id" {}
-variable "path_part" {}
-variable "methods" {
-  type = list(object({
-    http_method     = string
-    authorization   = string
-    integration_uri = string
-  }))
-}
-
-resource "aws_api_gateway_resource" "this" {
-  rest_api_id = var.rest_api_id
-  parent_id   = var.parent_id
-  path_part   = var.path_part
-}
-
-resource "aws_api_gateway_method" "this" {
-  for_each      = { for method in var.methods : method.http_method => method }
-  rest_api_id   = var.rest_api_id
-  resource_id   = aws_api_gateway_resource.this.id
-  http_method   = each.value.http_method
-  authorization = each.value.authorization
-}
-
-resource "aws_api_gateway_integration" "this" {
-  for_each                = { for method in var.methods : method.http_method => method }
-  rest_api_id             = var.rest_api_id
-  resource_id             = aws_api_gateway_resource.this.id
-  http_method             = each.value.http_method
-  type                    = "AWS_PROXY"
-  integration_http_method = "POST"
-  uri                     = each.value.integration_uri
-}
-
 resource "aws_api_gateway_rest_api" "notes_api" {
   name        = "Notes API"
   description = "API for Folder and Notes Services"
 }
 
 module "folder" {
-  source      = "."
+  source      = "./modules/api_gateway"
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = aws_api_gateway_rest_api.notes_api.root_resource_id
   path_part   = "folder"
@@ -68,7 +33,7 @@ module "folder" {
 }
 
 module "folder_id" {
-  source      = "."
+  source      = "./modules/api_gateway"
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = module.folder.aws_api_gateway_resource.this.id
   path_part   = "{folderId}"
@@ -87,7 +52,7 @@ module "folder_id" {
 }
 
 module "note" {
-  source      = "."
+  source      = "./modules/api_gateway"
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = aws_api_gateway_rest_api.notes_api.root_resource_id
   path_part   = "note"
@@ -106,7 +71,7 @@ module "note" {
 }
 
 module "note_id" {
-  source      = "."
+  source      = "./modules/api_gateway"
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = module.note.aws_api_gateway_resource.this.id
   path_part   = "{noteId}"
@@ -125,7 +90,7 @@ module "note_id" {
 }
 
 module "note_list" {
-  source      = "."
+  source      = "./modules/api_gateway"
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = module.note.aws_api_gateway_resource.this.id
   path_part   = "list"
