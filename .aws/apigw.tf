33bc24d1c961341539d73a85536f8b641d619721
Brian Moore <brianmoore0213@gmail.com>
2025-01-08 11:55:04 -0500

    small fix

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -7,7 +7,7 @@ variable "api_endpoints" {
 }
 
 locals {
-  endpoints = jsondecode(file("${path.module}/endpoints.json"))["endpoints"]
+  api_endpoints = jsondecode(file("${path.module}/endpoints.json"))["endpoints"]
 }
 
 resource "aws_apigatewayv2_api" "notes_api" {
@@ -17,7 +17,7 @@ resource "aws_apigatewayv2_api" "notes_api" {
 }
 
 resource "aws_apigatewayv2_integration" "integrations" {
-  for_each = local.endpoints
+  for_each = local.api_endpoints
 
   api_id             = aws_apigatewayv2_api.notes_api.id
   integration_type   = "AWS_PROXY"
@@ -26,7 +26,7 @@ resource "aws_apigatewayv2_integration" "integrations" {
 }
 
 resource "aws_apigatewayv2_route" "routes" {
-  for_each = local.endpoints
+  for_each = local.api_endpoints
 
   api_id    = aws_apigatewayv2_api.notes_api.id
   route_key = "${each.value.method} ${each.value.path}"
