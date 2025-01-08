d3c4d08534a34bb7d04ffbf03d521f316cd2c9b6
Brian Moore <brianmoore0213@gmail.com>
2025-01-08 12:39:21 -0500

    small fix

--- .aws/lambda.tf
+++ .aws/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "folder_service" {
   function_name    = "folder_service"
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.handler"
-  filename         = "./.serverless/noteService.zip"
+  filename         = "./.serverless/folderService.zip"
   source_code_hash = filebase64sha256("./.serverless/folderService.zip")
   runtime          = "nodejs18.x"
   timeout          = 10
