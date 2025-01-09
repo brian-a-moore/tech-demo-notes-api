5b98728770528ae2f60179fc5bb193d0da7379ab
Brian Moore <brianmoore0213@gmail.com>
2025-01-09 14:18:17 -0500

    fix

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -88,7 +88,7 @@ resource "aws_api_gateway_resource" "api_resources" {
   for_each    = local.api_endpoints
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = aws_api_gateway_rest_api.notes_api.root_resource_id
-  path_part   = each.value.path
+  path_part   = split("/", each.value.path)[length(split("/", each.value.path)) - 1]
 }
 
 resource "aws_api_gateway_method" "api_methods" {
