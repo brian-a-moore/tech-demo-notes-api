e31cdb1bfd2a806d0b8598130348c4ec467c81c4
Brian Moore <brianmoore0213@gmail.com>
2025-01-16 10:26:40 -0500

    rm dup code

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -99,15 +99,6 @@ resource "aws_api_gateway_integration" "create_folder_itegration" {
   uri                     = aws_lambda_function.notes_api.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "create_folder_itegration" {
-  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
-  resource_id             = aws_api_gateway_resource.folder_resource.id
-  http_method             = "POST"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.notes_api.invoke_arn
-}
-
 resource "aws_api_gateway_integration" "list_folder_itegration" {
   rest_api_id             = aws_api_gateway_rest_api.notes_api.id
   resource_id             = aws_api_gateway_resource.folder_resource.id
