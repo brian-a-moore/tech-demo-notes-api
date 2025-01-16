3de6203e16cd8eee37d746bba75a556d2833c030
Brian Moore <brianmoore0213@gmail.com>
2025-01-16 10:24:03 -0500

    decoupled apigw code

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -1,106 +1,180 @@
-locals {
-  resources = {
-    folder = "folder"
-    note   = "note"
-  }
-
-  folder_methods = {
-    create_folder = {
-      method = "POST"
-      path   = "/"
-    }
-    update_folder = {
-      method = "PUT"
-      path   = "/{folderId}"
-    }
-    list_folder = {
-      method = "GET"
-      path   = "/"
-    }
-    get_folder = {
-      method = "GET"
-      path   = "/{folderId}"
-    }
-    delete_folder = {
-      method = "DELETE"
-      path   = "/{folderId}"
-    }
-  }
-
-  note_methods = {
-    create_note = {
-      method = "POST"
-      path   = "/"
-    }
-    update_note = {
-      method = "PUT"
-      path   = "/{noteId}"
-    }
-    get_note = {
-      method = "GET"
-      path   = "/{noteId}"
-    }
-    delete_note = {
-      method = "DELETE"
-      path   = "/{noteId}"
-    }
-  }
-}
-
 resource "aws_api_gateway_rest_api" "notes_api" {
   name        = "notes_api"
   description = "API for Notes Application"
 }
 
-resource "aws_api_gateway_resource" "resources" {
-  for_each = local.resources
-
+resource "aws_api_gateway_resource" "folder_resource" {
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
   parent_id   = aws_api_gateway_rest_api.notes_api.root_resource_id
-  path_part   = each.value
+  path_part   = "folder"
 }
 
-resource "aws_api_gateway_resource" "folder_id" {
+resource "aws_api_gateway_resource" "note_resource" {
   rest_api_id = aws_api_gateway_rest_api.notes_api.id
-  parent_id   = aws_api_gateway_resource.resources["folder"].id
+  parent_id   = aws_api_gateway_rest_api.notes_api.root_resource_id
+  path_part   = "note"
+}
+
+resource "aws_api_gateway_resource" "folder_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.notes_api.id
+  parent_id   = aws_api_gateway_resource.folder_resource.id
   path_part   = "{folderId}"
 }
 
-resource "aws_api_gateway_method" "folder_methods" {
-  for_each = local.folder_methods
+resource "aws_api_gateway_resource" "note_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.notes_api.id
+  parent_id   = aws_api_gateway_resource.note_resource.id
+  path_part   = "{noteId}"
+}
 
+resource "aws_api_gateway_method" "create_folder_method" {
   rest_api_id   = aws_api_gateway_rest_api.notes_api.id
-  resource_id   = each.key == "get_folder" ? aws_api_gateway_resource.folder_id.id : aws_api_gateway_resource.resources["folder"].id
-  http_method   = each.value.method
+  resource_id   = aws_api_gateway_resource.folder_resource.id
+  http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "note_methods" {
-  for_each = local.note_methods
-
+resource "aws_api_gateway_method" "list_folder_method" {
   rest_api_id   = aws_api_gateway_rest_api.notes_api.id
-  resource_id   = aws_api_gateway_resource.resources["note"].id
-  http_method   = each.value.method
+  resource_id   = aws_api_gateway_resource.folder_resource.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "folder_integrations" {
-  for_each = local.folder_methods
+resource "aws_api_gateway_method" "delete_folder_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.folder_id_resource.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
 
+resource "aws_api_gateway_method" "get_folder_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.folder_id_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "update_folder_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.folder_id_resource.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "create_note_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.note_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "delete_note_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.note_id_resource.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_note_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.note_id_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "update_note_method" {
+  rest_api_id   = aws_api_gateway_rest_api.notes_api.id
+  resource_id   = aws_api_gateway_resource.note_id_resource.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "create_folder_itegration" {
   rest_api_id             = aws_api_gateway_rest_api.notes_api.id
-  resource_id             = each.key == "get_folder" ? aws_api_gateway_resource.folder_id.id : aws_api_gateway_resource.resources["folder"].id
-  http_method             = each.value.method
+  resource_id             = aws_api_gateway_resource.folder_resource.id
+  http_method             = "POST"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.notes_api.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "note_integrations" {
-  for_each = local.note_methods
-
+resource "aws_api_gateway_integration" "create_folder_itegration" {
   rest_api_id             = aws_api_gateway_rest_api.notes_api.id
-  resource_id             = aws_api_gateway_resource.resources["note"].id
-  http_method             = each.value.method
+  resource_id             = aws_api_gateway_resource.folder_resource.id
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "list_folder_itegration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.folder_resource.id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "delete_folder_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.folder_id_resource.id
+  http_method             = "DELETE"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_folder_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.folder_id_resource.id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "update_folder_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.folder_id_resource.id
+  http_method             = "PUT"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "create_note_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.note_resource.id
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "delete_note_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.note_id_resource.id
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_note_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.note_id_resource.id
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.notes_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "update_note_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.notes_api.id
+  resource_id             = aws_api_gateway_resource.note_id_resource.id
+  http_method             = "POST"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.notes_api.invoke_arn
