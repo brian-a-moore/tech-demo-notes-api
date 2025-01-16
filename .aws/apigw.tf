7adeda8ed610a921c82156b57035d00d23e363ca
Brian Moore <brianmoore0213@gmail.com>
2025-01-16 09:50:53 -0500

    fix duplicate resources

--- .aws/apigw.tf
+++ .aws/apigw.tf
@@ -60,11 +60,17 @@ resource "aws_api_gateway_resource" "resources" {
   path_part   = each.value
 }
 
+resource "aws_api_gateway_resource" "folder_id" {
+  rest_api_id = aws_api_gateway_rest_api.notes_api.id
+  parent_id   = aws_api_gateway_resource.resources["folder"].id
+  path_part   = "{folderId}"
+}
+
 resource "aws_api_gateway_method" "folder_methods" {
   for_each = local.folder_methods
 
   rest_api_id   = aws_api_gateway_rest_api.notes_api.id
-  resource_id   = aws_api_gateway_resource.resources["folder"].id
+  resource_id   = each.value.path == "/{folderId}" ? aws_api_gateway_resource.folder_id.id : aws_api_gateway_resource.resources["folder"].id
   http_method   = each.value.method
   authorization = "NONE"
 }
@@ -82,7 +88,7 @@ resource "aws_api_gateway_integration" "folder_integrations" {
   for_each = local.folder_methods
 
   rest_api_id             = aws_api_gateway_rest_api.notes_api.id
-  resource_id             = aws_api_gateway_resource.resources["folder"].id
+  resource_id             = each.value.path == "/{folderId}" ? aws_api_gateway_resource.folder_id.id : aws_api_gateway_resource.resources["folder"].id
   http_method             = each.value.method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
